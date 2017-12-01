a11d813c8c0ab8e3849fc33e8be249faa1b01802
Juechen Yin <juechen.yin0913@gmail.com>
2017-11-30 19:54:24 -0500

    bug fixes.

--- routes/campgrouds.js
+++ routes/campgrouds.js
@@ -3,18 +3,38 @@ var router = express.Router();
 var Campground = require('../model/campgroundSchema');
 var middlewareObj = require('../middleware/index');
 var geocoder = require('geocoder');
+const escapeRegexp = require('escape-string-regexp');
 // ========================
 // Campground Routes
 // ========================
 router.get('/', function (req, res) {
-    Campground.find({}, function (err, campgrounds) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(campgrounds);
-            res.render("campgrounds/index", {campgrounds: campgrounds});
-        }
-    });
+    if(req.query.search){
+        const escaptedString = escapeRegexp(req.query.search);
+        const regex = new RegExp(escaptedString,'gi');
+        Campground.find({name: regex}, function (err, campgrounds) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(campgrounds);
+                if(campgrounds.length < 1)
+                {
+                    // req.flash('error', "Can't find campground: " + regex + ", please try search other campgrounds or add a new one");
+                    res.redirect('campgrounds/index');
+                }else {
+                    res.render("campgrounds/index", {campgrounds: campgrounds});
+                }
+            }
+        });
+    }else {
+        Campground.find({}, function (err, campgrounds) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(campgrounds);
+                res.render("campgrounds/index", {campgrounds: campgrounds});
+            }
+        });
+    }
 });
 router.post("/", middlewareObj.loginCheck, function (req, res) {
     var name = req.body.name;
@@ -27,7 +47,7 @@ router.post("/", middlewareObj.loginCheck, function (req, res) {
     };
     geocoder.geocode(req.body.location, function (err, data) {
         console.log("data:" + data);
-        if(data.results[0])
+        if(data && data.results[0])
         {
             var lat = data.results[0].geometry.location.lat;
             var lng = data.results[0].geometry.location.lng;
@@ -81,19 +101,26 @@ router.put('/:id', middlewareObj.checkCampgroundPermission, function (req, res) 
     //find an update the correct campground
     //redirect somewhere
     geocoder.geocode(req.body.location, function (err, data) {
-        var lat = data.results[0].geometry.location.lat;
-        var lng = data.results[0].geometry.location.lng;
-        var location = data.results[0].formatted_address;
-        var newData = {name: req.body.name, image: req.body.image, description: req.body.description, price: req.body.price, location: location, lat: lat, lng: lng};
-        Campground.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, campground){
-            if(err){
-                req.flash("error", err.message);
-                res.redirect("back");
-            } else {
-                req.flash("success","Successfully Updated!");
-                res.redirect("/campgrounds/" + campground._id);
-            }
-        });
+        console.log(data);
+        if(data && data.results)
+        {
+            var lat = data.results[0].geometry.location.lat;
+            var lng = data.results[0].geometry.location.lng;
+            var location = data.results[0].formatted_address;
+            var newData = {name: req.body.name, image: req.body.image, description: req.body.description, price: req.body.price, location: location, lat: lat, lng: lng};
+            Campground.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, campground){
+                if(err){
+                    req.flash("error", err.message);
+                    res.redirect("back");
+                } else {
+                    req.flash("success","Successfully Updated!");
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }else {
+            req.flash("error", "Invalid location");
+            res.redirect('back');
+        }
     });
 });
 
@@ -104,4 +131,7 @@ router.delete('/:id', middlewareObj.checkCampgroundPermission, function (req, re
     });
 });
 
+function escapeRegex(text) {
+    return text.replace();
+}
 module.exports = router;
